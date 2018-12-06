43d8c9e05c5d364f0720724906d0aba309995ae1
Rupom Ahsan <qxv4870@sfap.asiapacific.bmw.corp>
2018-12-06 18:28:46 +0800

    first commit

---
+++ CFEApi/Model/CFEDbContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CFEApi.Model
+{
+  public class CFEDbContext : DbContext
+  {
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    // private const string CONN =
+    //               @"Server=Localhost;
+    //                 Database=PTC-Pluralsight;
+    //                 Trusted_Connection=True;
+    //                 MultipleActiveResultSets=true";
+
+    private const string CONN = @"Data Source=SSMY0099\DEV_MY_APPS;Initial Catalog=LoyaltyPlus; Integrated Security=true; MultipleActiveResultSets=true";
+
+    protected override void OnConfiguring(
+                DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer(CONN);
+    }
+  }
+}
